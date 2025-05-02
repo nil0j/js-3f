a8f2fcbd909a19cb90a8aea81d5279090ca153dd
niliaranet <niliara@proton.me>
2025-05-02 18:17:04 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import './App.css'
 
 
@@ -9,99 +9,164 @@ function App() {
         setTheme(theme == "light" ? "dark" : "light")
 
     const [users, setUsers] = useState([])
-    const [currentUser, setCurrentUser] = useState(0)
+    const [currentUserId, setCurrentUserId] = useState(undefined)
+    const currentUser = users.find(user => user.id == currentUserId);
 
-    function addUser(name) {
-        setCount(c => { c++ })
+    function addUser(input) {
+        let name = input.value
+        if (name == "") return
+        setCount(c => c + 1)
         setUsers((prevUsers) => [...prevUsers, { id: count, name: name, tasks: [] }]);
+        input.value = ""
     }
 
     function selectUser(user) {
-        setCurrentUser(user)
+        setCurrentUserId(user.id)
     }
 
     function deselectUser() {
-        setCurrentUser(undefined)
+        setCurrentUserId(undefined)
     }
 
     function addTask(task) {
-        console.log(task)
-        console.log(users)
+        if (task == "") return
         setUsers((prevUsers) => {
-            const updatedUsers = prevUsers.map((user) => {
-                if (user.id == currentUser) {
+            return prevUsers.map((user) => {
+                if (user.id == currentUserId) {
                     return { ...user, tasks: [...user.tasks, task] };
                 }
                 return user;
             });
-            return updatedUsers;
         });
     }
 
+    function deleteTask(index) {
+        setUsers((prevUsers) => {
+            return prevUsers.map((user) => {
+                if (user.id == currentUserId) {
+                    return {
+                        ...user, tasks: currentUser.tasks.filter((_, i) => i != index)
+                    };
+                }
+                return user;
+            });
+        });
+    }
+
+    function editTask(index) {
+        let value = prompt("ALBERT ESCRIBE VAVAVAVA QUE SE ACABA EL TIEMPO")
+        if (value == "") return
+        setUsers((prevUsers) => {
+            return prevUsers.map((user) => {
+                if (user.id == currentUserId) {
+                    return {
+                        ...user, tasks: currentUser.tasks.map((t, i) => i == index ? value : t)
+                    };
+                }
+                return user;
+            });
+        });
+    }
 
     return (
         <div className={theme}>
             <div className="app">
-                <aside className="sidebar card">
-                    <h2>Usuarios</h2>
-                    <ul id="userList">
-                        {users.map((user, index) => (
-                            <li key={index} style={{ cursor: "pointer" }} onClick={_ => selectUser(user)}>
-                                {user.name}
-                            </li>
-                        ))}
-                    </ul>
-                    <form action="" onSubmit={(e) => {
-                        e.preventDefault()
-                        let input = e.target.querySelector(".text")
-                        addUser(input.value)
-                    }}>
-                        <input type="text" id="newUserInput" className="text" placeholder="Nuevo usuario..." /><br />
-                        <input type="submit" value="Añadir Usuario" />
-                    </form>
-
-                    {
-                        currentUser == 0 ?
-                            <></>
-                            :
-                            <div id="userInfo">
-                                <hr />
-                                <p id="userName">{currentUser.name}</p>
-                                <p id="userStats"></p>
-                                <button onClick={deselectUser}>Deseleccionar</button>
-                            </div>
-                    }
-
-                    <button onClick={toggleTheme} style={{ "marginTop": "auto" }}>🌙/☀️ Tema</button>
-                </aside>
-                <main className="main">
-                    <div className="card">
-                        <h1 id="mainTitle">{currentUser ? currentUser.name : "Selecciona un usuario"}</h1>
-                        {currentUser ? (
-                            <div id="taskSection">
-                                <form action="" onSubmit={(e) => {
-                                    e.preventDefault()
-                                    let newTaskInput = document.getElementById("newTaskInput")
-                                    addTask(newTaskInput.value)
-                                    newTaskInput.value = ""
-                                }}>
-                                    <ul id="taskList">
-                                        <li>task.name</li>
-                                        <li>adsasd -&gt; {currentUser.tasks}</li>
-                                        {currentUser.tasks.map((task, index) => (
-                                            <li>task.name</li>
-                                        ))}
-                                    </ul>
-                                    <input type="text" id="newTaskInput" placeholder="Nueva tarea..." />
-                                    <input type="submit" value="Añadir Tarea" />
-                                </form>
-                            </div>
-                        ) : <></>}
-                    </div>
-                </main>
+                <Sidebar
+                    users={users}
+                    addUser={addUser}
+                    selectUser={selectUser}
+                    deselectUser={deselectUser}
+                    currentUser={currentUser}
+                    toggleTheme={toggleTheme}
+                />
+                <Main
+                    currentUser={currentUser}
+                    addTask={addTask}
+                    editTask={editTask}
+                    deleteTask={deleteTask}
+                />
             </div>
         </div >
     )
 }
+
+function Sidebar({ users, addUser, selectUser, deselectUser, currentUser, toggleTheme }) {
+    return <aside className="sidebar card">
+        <h2>Usuarios</h2>
+        <ul id="userList">
+            {users.map((user, index) => (
+                <li key={index} style={{ cursor: "pointer" }} onClick={_ => selectUser(user)}>
+                    {user.name}
+                </li>
+            ))}
+        </ul>
+        <form action="" onSubmit={(e) => {
+            e.preventDefault()
+            let input = e.target.querySelector(".text")
+            addUser(input)
+        }}>
+            <input style={{ "width": "100%", "boxSizing": "border-box" }} type="text" id="newUserInput" className="text" placeholder="Nuevo usuario..." /><br />
+            <input style={{ "width": "100%" }} type="submit" value="Añadir Usuario" />
+        </form>
+
+        {
+            currentUser != undefined ?
+                <div id="userInfo">
+                    <hr />
+                    <p id="userName">{currentUser.name}</p>
+                    <p id="userStats"></p>
+                    <button onClick={deselectUser}>Deseleccionar</button>
+                </div>
+                :
+                <></>
+        }
+
+        <button onClick={toggleTheme} style={{ "marginTop": "auto" }}>🌙/☀️ Tema</button>
+    </aside>
+
+}
+
+function Main({
+    currentUser,
+    addTask,
+    editTask,
+    deleteTask,
+}) {
+    return <main className="main">
+        <div className="card">
+            <h1 id="mainTitle">{currentUser ? currentUser.name : "Selecciona un usuario"}</h1>
+            {currentUser !== undefined ? (
+                <div id="taskSection">
+                    <form action="" onSubmit={(e) => {
+                        e.preventDefault()
+                        let newTaskInput = document.getElementById("newTaskInput")
+                        addTask(newTaskInput.value)
+                        newTaskInput.value = ""
+                    }}>
+                        <TaskList currentUser={currentUser} editTask={editTask} deleteTask={deleteTask} />
+                        <input type="text" id="newTaskInput" placeholder="Nueva tarea..." />
+                        <input type="submit" value="Añadir Tarea" />
+                    </form>
+                </div>
+            ) : <></>}
+        </div>
+    </main>
+}
+
+function TaskList({ currentUser, editTask, deleteTask }) {
+    return <ul id="taskList">
+        {currentUser.tasks.map((task, index) => (
+            <li key={index}>
+                <span>{task}</span>
+                <div className="actions">
+                    <button style={{ 'display': 'none' }}>✏️</button>
+                    <button onClick={() => { editTask(index) }}>✏️</button>
+                    <button onClick={() => { deleteTask(index) }}>🗑️</button>
+                </div>
+            </li>
+        ))}
+    </ul>
+}
+
 
 export default App
